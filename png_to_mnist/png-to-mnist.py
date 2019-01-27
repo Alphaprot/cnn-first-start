a0697c6c5a2c0b0989109566c4af106b3745428a
Yannik <36708997+Alphaprot@users.noreply.github.com>
2019-01-27 21:51:26 +0100

    Changed image names to represent label and number

--- png_to_mnist/png-to-mnist.py
+++ png_to_mnist/png-to-mnist.py
@@ -3,18 +3,21 @@ import matplotlib.pyplot as plt
 import os
 import numpy as np
 from PIL import Image
+from scipy import ndimage
 import glob
 
 image_x_size = 28
 image_y_size = 28
 image_list = []
-image_path = "image_store"
+xImage = None # Leere Variable (spaeter numpy-Array fuer Bilddaten)
+yLabel = None
+scriptPath = os.path.dirname(os.path.abspath(__file__))
 
 
 #image_store = os.listdir("test-images/")
 
-def show():
-    for filename in glob.glob('test-images/*.png'):
+def createLists():
+    for filename in glob.glob(scriptPath + '/test-images/*.png'):
         img=Image.open(filename)
         image_list.append(img)
 
@@ -22,6 +25,8 @@ def show():
         if len(image_list) == 0:
             print ("No files were read! Please check input directory!") #images_plt = [Image.open(i) for i in range(len(image_store)) if i.endswith(".png")]
 
+    xImage = np.zeros((len(image_list), 784)) # Numpy-Array mit Laenge aller Bilder in der image_list
+    yLabel = np.zeros((len(image_list),10)) # Numpy-Array mit Labels 0-9
 
     print(*image_list, sep='\n')
     print("\n", len(image_list), " images were read.")
@@ -31,10 +36,16 @@ def convert():
         img = Image.open(filename)
         img.convert("LA")
         width, height = images_plt.size
-        print (filename," size is now",width," by ",height)
 
+        if (width > image_x_size or height > image_y_size):
+            Image.resize(image_x_size, image_y_size)
+            print (filename," size is now",width," by ",height)
 
-show()
+def makeLabels():
+    for filename in image_list:
+        return filename
+
+createLists()
 convert()
 """
     image = Image.open(file)
