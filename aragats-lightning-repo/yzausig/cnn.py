b4b09198f033d5fa07f298b51ff3a8741ae21fb0
Yannik <36708997+Alphaprot@users.noreply.github.com>
2019-06-03 20:15:25 +0200

    Add files via upload

--- aragats-lightning-repo/yzausig/cnn.py
+++ aragats-lightning-repo/yzausig/cnn.py
@@ -7,6 +7,7 @@ import json
 import math
 import tensorflow as tf
 from PIL import Image, ImageChops
+from getch import getch, pause
 
 json_config = "03_img.json"
 
@@ -34,6 +35,10 @@ def removeBackground():
 
     filelist = data['filelist']
     seqlist = data['seqlist']
+    counter = 0
+
+    if not os.path.exists(imgpath + "/bg_substraction"):
+        os.makedirs(imgpath + "/bg_substraction")
 
     for seq in seqlist:
         ref = seq['ref']
@@ -45,19 +50,47 @@ def removeBackground():
         #extensionlist.append(seq['ref'])
         #imgnumber = int(''.join(str(extensionlist)))
 
-        refimg = imgpath + "/aragats-%04d.jpg" %ref
+        refimg = imgpath + "/aragats-%04d.jpg" % ref
         print(imgpath)
         if(os.path.isfile(refimg)):
             img_compare = Image.open(refimg)
-            print("Opening image no. %04d") %ref
 
-                for img in seq["images"]:
-                    infile = imgpath + "aragats-%04d.jpg" %img
-                    
+            for counter, img in enumerate(seq['images']):
+                print(type(img))
+                print(str("Opening image no. %04d\n") % img)
+                infile = imgpath + "/aragats-%04d.jpg" % img
+                inv_outfile = imgpath + "/bg_substraction/" + "1_aragats-%04d.jpg" % img
+                input = Image.open(infile)
+                process = ImageChops.subtract(input, img_compare)
+                out = ImageChops.invert(process)
+                out.save(inv_outfile)
+
 
         else:
             print("Invalid format or file not found!\n")
 
+    print(str(counter) + " image files have been successfully converted!\n")
+    print("The results have been saved to " + imgpath + "/bg_substraction/\n")
+    print("")
+    question = "Do you want to train the CNN with this data ((N) will quit the program without training)?"
+    prompt = " [y/n] "
+    choice = raw_input().lower()
+    validInput = {"yes": True, "y": True,
+                    "no": False, "n": False}
 
+    while True:
+        print(question + prompt)
+        if choice in validInput:
+            return valid[choice]
+        else:
+            print("Please respond with 'yes' or 'no' (or 'y' or 'n')\n")
+
+    if validInput == True:
+        trainCNN();
+    if validInput == False:
+        exit()
+
+def trainCNN():
+    print("Nothing here")
 
 removeBackground()
