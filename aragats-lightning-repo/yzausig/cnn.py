1535b7243bbc2b4e67b34c2a696177392ed6e700
Yannik <36708997+Alphaprot@users.noreply.github.com>
2019-06-24 16:30:46 +0200

    Add files via upload

--- aragats-lightning-repo/yzausig/cnn.py
+++ aragats-lightning-repo/yzausig/cnn.py
@@ -10,6 +10,7 @@ from PIL import Image, ImageChops
 from getch import getch, pause
 
 json_config = "03_img.json"
+desiredArrayShape = '(3,3)'
 
 def removeBackground():
     print("Please specify input directory as an abs path:\n")
@@ -19,6 +20,7 @@ def removeBackground():
     valid_path = os.path.isdir(imgpath)
     while not valid_path:
         print("Invalid path. Please specify a valid path")
+        exit()
     else:
         print("Input directory is now" + imgpath + "\n")
 
@@ -72,25 +74,60 @@ def removeBackground():
     print(str(counter) + " image files have been successfully converted!\n")
     print("The results have been saved to " + imgpath + "/bg_substraction/\n")
     print("")
-    question = "Do you want to train the CNN with this data ((N) will quit the program without training)?"
-    prompt = " [y/n] "
-    choice = raw_input().lower()
-    validInput = {"yes": True, "y": True,
-                    "no": False, "n": False}
 
-    while True:
-        print(question + prompt)
-        if choice in validInput:
-            return valid[choice]
+def trainDataset():
+    if trainingData.shape
+def loadTrainData():
+    actionComplete = False
+    while not actionComplete():
+        print("Please specify the abs path to the training data file:\n")
+        trainFile = raw_input()
+        validTrainFile = os.path.isfile(trainFile)
+        if not validTrainFile:
+            print("Train data file does not exist. Please check the file or correct the path")
         else:
-            print("Please respond with 'yes' or 'no' (or 'y' or 'n')\n")
-
-    if validInput == True:
-        trainCNN();
-    if validInput == False:
-        exit()
+            if trainFile.endswith(".npy"):
+                print("Selected training file: " + trainFile + "\n")
+                global trainingData = np.load(trainFile)
+                actionComplete = True
+                break()
+            else:
+                print("Please select a valid '.npy' file!")
+    trainCNN()
 
 def trainCNN():
     print("Nothing here")
 
 removeBackground()
+question = "Do you want to:\n"
+option1 = "[1] Create training data from the converted images\n"
+option2 = "[2] Train neural network by providing existing training data as .npy\n"
+option3 = "[3] Display images\n"
+options = [option1, option2, option3]
+
+
+prompt = " [Enter digit] "
+choice = raw_input().lower()
+validInput = {"1":1,
+                "2":2,
+                "3":3}
+
+while True:
+    print(*options + prompt)
+    if choice in validInput:
+        return valid[choice]
+    else:
+        print("Please respond with 'yes' or 'no' (or 'y' or 'n')\n")
+
+if validInput == 1:
+    trainDataset();
+if validInput == 2;
+    loadTrainData();
+if validInput == 3:
+    exit()
+
+
+'''    validInput = {"1": True, "y": True,
+                    "no": False, "n": False}
+                            print("Please respond with 'yes' or 'no' (or 'y' or 'n')\n")
+'''
